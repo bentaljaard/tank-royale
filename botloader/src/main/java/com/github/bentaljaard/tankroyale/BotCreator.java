3a86b7b05699df4efa6b61b4d6fd0e7b6bf4affe
Ben Taljaard <benfrancotaljaard@gmail.com>
2022-07-22 13:43:50 +0200

    refactor create to now use base64 code files for bot, also load from template when none is specified

--- botloader/src/main/java/com/github/bentaljaard/tankroyale/BotCreator.java
+++ botloader/src/main/java/com/github/bentaljaard/tankroyale/BotCreator.java
@@ -3,6 +3,7 @@ package com.github.bentaljaard.tankroyale;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,39 +27,30 @@ public class BotCreator {
 
     @ConfigProperty(name = "bot.version") String botVersion;
     @ConfigProperty(name = "bot.server.url") String botUrl;
-    String botCode;
     
     public BotCreator() {
-        
-        context = new ResourceDefinitionContext.Builder()
-            .withNamespaced(false)
-            .withKind("TankRoyaleBot")
-            .withGroup("tank-royale.quay.io")
-            .withVersion("v1alpha1")
-            .withPlural("TankRoyaleBots")
-            .build();
+
     }
 
-
-
-    public boolean createBot(String name, String code){
-        String currentNamespace = oc.getNamespace();
-        oc.namespaces().list();
-
-        MixedOperation<TankRoyaleBot,KubernetesResourceList<TankRoyaleBot>,Resource<TankRoyaleBot>> botClient = 
-            oc.resources(TankRoyaleBot.class);
-
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream codeResource = (InputStream) loader.getResourceAsStream("ExternalCodeBot.java_template");
-        byte[] text = null;
-        try {
-            text = codeResource.readAllBytes();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public boolean createBot(String name, String encodedCode){
+        if (encodedCode == null || encodedCode.isBlank()){
+            //Load default code template
+            encodedCode = loadBotCodeTemplate("ExternalCodeBot.java_template");
         }
-        botCode = new String(text, StandardCharsets.UTF_8);
-        
+
+        //Decode base64 encoded code
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCode);
+        String botCode = new String(decodedBytes, StandardCharsets.UTF_8);
+
+        //Use current namespace
+        String currentNamespace = oc.getNamespace();
+        // oc.namespaces().list();
+
+        //Create an API client for the CR
+        MixedOperation<TankRoyaleBot,KubernetesResourceList<TankRoyaleBot>,Resource<TankRoyaleBot>> botClient = 
+        oc.resources(TankRoyaleBot.class);
+
+        //Build out the CR definition
         Map<String,String> botDefinition = new HashMap<String,String>();
         botDefinition.put("name", name);
         botDefinition.put("version", botVersion);
@@ -66,10 +58,11 @@ public class BotCreator {
 
         Map<String,String> serverDefinition = new HashMap<String,String>();
         serverDefinition.put("url",botUrl);
-       
         
+        //Create new spec from our maps
         TankRoyaleBotSpec botSpec = new TankRoyaleBotSpec(botDefinition,serverDefinition);
 
+        //Create the CR
         botClient.inNamespace(currentNamespace)
             .createOrReplace(new TankRoyaleBot(name, botSpec));
 
@@ -78,6 +71,19 @@ public class BotCreator {
         
 
         return true;
+    }
+
+    public String loadBotCodeTemplate(String resourceName){
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream codeResource = (InputStream) loader.getResourceAsStream("ExternalCodeBot.java_template");
+        byte[] text = null;
+        try {
+            text = codeResource.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String botCode = Base64.getEncoder().encodeToString(text);
+        return botCode;
     }
 
     
--- botloader/src/main/java/com/github/bentaljaard/tankroyale/BotResource.java
+++ botloader/src/main/java/com/github/bentaljaard/tankroyale/BotResource.java
@@ -1,7 +1,11 @@
 package com.github.bentaljaard.tankroyale;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -21,7 +25,15 @@ public class BotResource {
         return config;
     }
 
-    //TODO: list all bots (get CRs in namespace)
+    @Path("/bot")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<BotConfig> listBots(){
+        //TODO: list all bots (get CRs in namespace)
+        return new ArrayList<BotConfig>();
+    }
+
+    
     //TODO: Remove bots (cleanup indiviual or all)
     //TODO: Update bot (eg. update code)
 }
